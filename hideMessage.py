d79a5696af2077a1305bad52504cbaa83cafda2d
npoudelp <npoudelp@gmail.com>
2023-05-01 18:37:50 +0545

    image is handeled with PIL and R G B color code for each pixel is extracted and stored in var R G B respectively

--- hideMessage.py
+++ hideMessage.py
@@ -1,7 +1,19 @@
 import time
+import numpy as np
+from PIL import Image
+import sys
+import codecs
 
 
-def hide():
-    print("Hiding... haha")
-    time.sleep(2)
+def getText():
+    text = input("Enter the message: \n")
+    return text
 
+
+def hide(imageName):
+    img = Image.open(imageName)
+    for y in range(img.size[0]):
+        for x in range(img.size[1]):
+            r, g, b = img.getpixel((x, y))
+            print(r, g, b)
+    input("Holding...")
