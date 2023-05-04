d2bad4ec9fce11fcf4b24942f44cbaef0ccdde6a
npoudelp <npoudelp@gmail.com>
2023-05-04 10:15:50 +0545

    loop init var error corrected

--- showMessgae.py
+++ showMessgae.py
@@ -1,5 +1,5 @@
 import time
-import selectImage as si
+
 from PIL import Image
 
 escSeq = "001000000101101101000101010011100100010001011101"
@@ -12,33 +12,25 @@ def show(imageName):
     binText = ""
     img = Image.open(imageName)
 
-    print(len(escSeq))
+    # print(len(escSeq))
 
-    for x in range(img.size[0]):
+    for y in range(img.size[1]):
         if len(binText) >= 42:
             if binText[-42:] == escSeq:
                 break
-        for y in range(img.size[1]):
+        for x in range(img.size[0]):
             r, g, b = img.getpixel((x, y))
             binR = f"{r:08b}"
             binText = binText + binR[6:]
+            # print(binText)
+            print(binR)
             if len(binText) >= 48:
-                if binText[-42:] == escSeq:
+                if binText[-48:] == escSeq:
                     break
+
+            input("Holding...")
+
     print(binText)
-    startText = 0
-    endText = startText + 8
+    input("Holding...")
 
-    decodedMessage = ""
-    for bits in binText:
-        slicedBits = binText[startText: endText]
-        startText = startText + 8
-        endText = startText + 8
-        desSlicedBits = int(slicedBits, 2)
-        print(slicedBits)
-        print(desSlicedBits)
-
-        input("Holding...")
-
-
-# show("/home/asp/aaa.jpg")
+show("/aaa.jpeg")
