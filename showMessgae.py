fef276342f783787c11fb7bf7f9e93ff32ddcc9c
npoudelp <npoudelp@gmail.com>
2023-05-05 12:10:24 +0545

    text from image extracted and displayed to the terminal, vim style option to add latter

--- showMessgae.py
+++ showMessgae.py
@@ -1,36 +1,63 @@
 import time
 
+import intro
+import selectImage as si
 from PIL import Image
 
 escSeq = "001000000101101101000101010011100100010001011101"
 
 
+def hold():
+    checkToExit = input("\nEnter '!q' to exit: ")
+    if checkToExit == '!q':
+        pass
+    else:
+        hold()
+
+
+def display(text):
+    return text
+
+
 def show(imageName):
     global escSeq
+    si.clear()
+    intro.showBrand()
 
-    print(imageName)
+    print(f"Selecting image {imageName}\n")
     binText = ""
     img = Image.open(imageName)
 
-    # print(len(escSeq))
-
     for y in range(img.size[1]):
-        if len(binText) >= 42:
-            if binText[-42:] == escSeq:
+        if len(binText) >= 48:
+            if binText[-48:] == escSeq:
                 break
         for x in range(img.size[0]):
             r, g, b = img.getpixel((x, y))
             binR = f"{r:08b}"
             binText = binText + binR[6:]
-            # print(binText)
-            print(binR)
             if len(binText) >= 48:
                 if binText[-48:] == escSeq:
                     break
 
-            input("Holding...")
+    startText = 0
+    endText = startText + 8
+    decodedMessage = ""
 
-    print(binText)
-    input("Holding...")
+    loopBinText = len(binText) / 8
+    loopCount = 0
+    while loopCount < loopBinText:
+        slicedBits = binText[startText: endText]
+        startText = startText + 8
+        endText = startText + 8
+        desSlicedBits = int(str(slicedBits), 2)
+        decodedMessage = decodedMessage + chr(desSlicedBits)
+        loopCount = loopCount + 1
 
-show("/aaa.jpeg")
+    print("Message extracted successfully...")
+    time.sleep(0.5)
+    si.clear()
+    intro.showBrand()
+    print("\n\tYour message ends with [END]\n")
+    print(display(decodedMessage))
+    hold()
