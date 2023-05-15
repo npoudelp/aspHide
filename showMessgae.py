b010d64ea75af698ea3a0cd9d9bfc7f636de2da6
npoudelp <npoudelp@gmail.com>
2023-05-15 17:21:30 +0545

    password is obtained from users, matched it with the password used to authenticate the image by encoder and only shown message if password matches. feature added :)

--- showMessgae.py
+++ showMessgae.py
@@ -6,8 +6,10 @@ from intro import showBrand
 import selectImage as si
 from PIL import Image
 
-escSeq = "001000000101101101000101010011100100010001011101"
-
+escSeqForMessage = "001000000101101101000101010011100100010001011101"
+escSeqForPassword = "01011011011110110101111001011101"
+passwordFlag = 0
+wrongCount = 0
 fileExp = ""
 
 
@@ -22,7 +24,7 @@ def writeTextToFile(text):
 def hold(text):
     checkToExit = input("\nEnter 'q!' to exit without saving and 'w!' to save and exit: ")
     if checkToExit == 'q!':
-        pass
+        return
     elif checkToExit == 'w!':
         writeTextToFile(text)
         si.clear()
@@ -36,50 +38,100 @@ def display(text):
     return text
 
 
-def show(imageName):
-    global escSeq
-    global imgName
-    imgName = imageName
-    si.clear()
+def checkPassword():
+    global escSeqForPassword, imgName, passwordFlag, wrongCount
+    if passwordFlag != 0:
+        si.clear()
+        print("Wrong password\n")
+    password = input("Password (for main menu: [MAIN]): ")
+    password = password + '[{^]'
+    encodedPassword = ""
+    if password == '[MAIN][{^]':
+        return '[MAIN]'
+    else:
+        passwordFromImage = ""
+        for char in password:
+            acsiiOfChar = ord(char)
+            binOfChar = f"{acsiiOfChar:08b}"
+            encodedPassword = encodedPassword + binOfChar
 
-    print(f"Selecting image {imageName}\n")
-    binText = ""
-    img = Image.open(imageName)
-
-    for y in range(img.size[1]):
-        if len(binText) >= 48:
-            if binText[-48:] == escSeq:
-                break
-        for x in range(img.size[0]):
-            r, g, b = img.getpixel((x, y))
-            binR = f"{r:08b}"
-            binText = binText + binR[6:]
-            if len(binText) >= 48:
-                if binText[-48:] == escSeq:
+        img = Image.open(imgName)
+        for y in range(img.size[1]):
+            if len(passwordFromImage) >= 32:
+                if passwordFromImage[-32:] == escSeqForPassword:
                     break
+            for x in range(img.size[0]):
+                r, g, b = img.getpixel((x, y))
+                binR = f'{r:08b}'
+                passwordFromImage = passwordFromImage + binR[6:]
+                if len(passwordFromImage) >= 32:
+                    if passwordFromImage[-32:] == escSeqForPassword:
+                        break
 
-    startText = 0
-    endText = startText + 8
-    decodedMessage = ""
+        if passwordFromImage == encodedPassword:
+            return '[YES]'
+        else:
+            passwordFlag = 1
+            wrongCount = wrongCount + 1
+            if wrongCount >= 3:
+                return '[NO]'
+            checkPassword()
 
-    loopBinText = len(binText) / 8
-    loopCount = 0
-    while loopCount < loopBinText:
-        slicedBits = binText[startText: endText]
-        startText = startText + 8
-        endText = startText + 8
-        desSlicedBits = int(str(slicedBits), 2)
-        decodedMessage = decodedMessage + chr(desSlicedBits)
-        loopCount = loopCount + 1
 
-    print("Message extracted successfully...")
-    time.sleep(2)
+def show(imageName):
+    global escSeqForMessage, passwordFlag, wrongCount, imgName
+    imgName = imageName
+    passwordFlag = 0
+    wrongCount = 0
     si.clear()
-    os.chdir(f"{os.path.dirname(__file__)}")
-    showBrand()
-    print("\nYour message ends with [END]\n")
-    print(display(decodedMessage))
-    hold(decodedMessage)
+
+    status = checkPassword()
+
+    if status == '[YES]':
+        print(f"Selecting image {imageName}\n")
+        binText = ""
+        img = Image.open(imageName)
+
+        for y in range(img.size[1]):
+            if len(binText) >= 48:
+                if binText[-48:] == escSeqForMessage:
+                    break
+            for x in range(img.size[0]):
+                r, g, b = img.getpixel((x, y))
+                binR = f"{r:08b}"
+                binText = binText + binR[6:]
+                if len(binText) >= 48:
+                    if binText[-48:] == escSeqForMessage:
+                        break
+
+        startText = 0
+        endText = startText + 8
+        decodedMessage = ""
+
+        loopBinText = len(binText) / 8
+        loopCount = 0
+        while loopCount < loopBinText:
+            slicedBits = binText[startText: endText]
+            startText = startText + 8
+            endText = startText + 8
+            desSlicedBits = int(str(slicedBits), 2)
+            decodedMessage = decodedMessage + chr(desSlicedBits)
+            loopCount = loopCount + 1
+
+        print("Message extracted successfully...")
+        time.sleep(1)
+        si.clear()
+        os.chdir(f"{os.path.dirname(__file__)}")
+        showBrand()
+        print("\nYour message ends with [END]\n")
+        print(display(decodedMessage))
+        hold(decodedMessage)
+
+    elif status == '[NO]':
+        name = input("Hold: ")
+        print(name)
+    else:
+        print("\nUnknown error occurred...")
 
 
 message = ""
